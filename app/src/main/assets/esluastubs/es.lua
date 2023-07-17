8e242b5491c2267da89e8e7487d2bb6f2dcfa086
ThatMG393 <thatmg393@gmail.com>
2023-07-17 13:06:07 +0800

    add missing files

---
+++ app/src/main/assets/esluastubs/es.lua
@@ -0,0 +1,24 @@
+Multiblock = {
+    Type = nil
+    
+	Rigidbody = {
+        UseGravity = true
+    }
+    
+    ModBlock = {
+        function Send(d) end
+    }
+}
+
+-- Multiblock
+function TryGetMultiBlockFromConnection(connectionIndex, isIncoming, count) end
+function 
+    
+-- Sound
+function SetSoundVolume(volume) end
+function SetSoundLoop(value) end
+function PlaySound(soundPath) end
+
+-- Data
+function GetBool(key, value) end
+function GetFloat(key, value) end
